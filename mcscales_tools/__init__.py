179ab14d5e71df1f517914c716b814b993eaa21f
Zahari <zaharid@gmail.com>
2021-08-21 23:28:33 +0100

    Remove unneeded check for LHAPDF

--- mcscales_tools/__init__.py
+++ mcscales_tools/__init__.py
@@ -1,17 +1,2 @@
 """Tools for wroking with LHAPDF grids with MCScales (arxiv:xxxx)"""
-__version__ = "1.0"
-
-try:
-    import lhapdf
-except ImportError as e:
-    raise ImportError(
-        """The LHAPDF python wrappers could not be found in the environment. We suggest installing them using the conda  system with the command:
-
-    conda install lhapdf -c https://packages.nnpdf.science/public
-
-Alternaitvely, please check
-
-    https://lhapdf.hepforge.org/install.html
-
-for instrctions."""
-    ) from e
+__version__ = "0.1"
